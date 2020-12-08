8bde7fe1245b281fd1b7e74cb810e75d216eda9b
William Bugenis <wbugenis@gmail.com>
2020-12-08 21:20:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,9 +84,9 @@ def adding_matz
     programmer_hash[:yukihiro_matsumoto] = {
      :known_for => "Ruby",
      :languages => ["LISP", "C"]
-     }
+    }
      
-     programmer_hash
+    programmer_hash
 end
 
 def changing_alan
